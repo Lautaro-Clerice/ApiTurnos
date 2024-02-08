d804e94880227ff588eb34795703e3c9f5bd1e7f
Lautaro <clericelautaro@gmail.com>
2024-02-07 22:07:44 -0300

    act cors

--- models/server.ts
+++ models/server.ts
@@ -1,39 +1,44 @@
-import express, {Express} from 'express'
-import cors from 'cors'
-import authRoutes from '../routes/auth'
-import turnosRoutes from '../routes/turnos'
-import { dbConnection } from '../database/config'
+import express, { Express } from 'express';
+import cors from 'cors';
+import authRoutes from '../routes/auth';
+import turnosRoutes from '../routes/turnos';
+import { dbConnection } from '../database/config';
+
 export class Server {
-    app: Express
-    port: string | number | undefined
-    authPath: string
-    turnosPath: string
+    app: Express;
+    port: string | number | undefined;
+    authPath: string;
+    turnosPath: string;
+
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        this.authPath = '/auth'
-        this.turnosPath = '/turnos'
-        this.conectarDB()
-        this.middlewares()
-        this.routes()
+        this.authPath = '/auth';
+        this.turnosPath = '/turnos';
+        this.conectarDB();
+        this.middlewares();
+        this.routes();
     }
+
     async conectarDB(): Promise<void>  {
-         await dbConnection();
+        await dbConnection();
     }
+
     routes(): void {
-        this.app.use(this.authPath, authRoutes)
-        this.app.use(this.turnosPath, turnosRoutes)
+        this.app.use(this.authPath, authRoutes);
+        this.app.use(this.turnosPath, turnosRoutes);
     }
 
     middlewares(): void {
         this.app.use(express.json());
-        this.app.use(cors())
+        this.app.use(cors({
+            origin: 'http://localhost:3000'
+        }));
     }
 
     listen(): void {
         this.app.listen(this.port, () => {
-            console.log(`corriendo en puerto${this.port}`);
-            
-        })
+            console.log(`Servidor corriendo en puerto ${this.port}`);
+        });
     }
 }
